45ce27843abd485f3ea1c6370857523fe8ef8b53
TJ Mazeika <mazeika.tj@gmail.com>
2021-10-19 13:27:21 -0400

    Formatting

--- src/json/mapCodec.ts
+++ src/json/mapCodec.ts
@@ -5,8 +5,7 @@ import { TypeCodec } from './index';
  * Supports ES6 Map types.
  */
 export const mapCodec: TypeCodec<Map<unknown, unknown>,
-  [unknown, unknown][]
-> = {
+  [unknown, unknown][]> = {
   schema: $array($tuple($unknown(), $unknown())).thenMap((v) => new Map(v)),
   isType: (v): v is Map<unknown, unknown> => v instanceof Map,
   unwrap: (v) => Array.from(v.entries()),
